274d77bb536ee32c13135dc70dd0a2ebf902522b
ogawashintaro <ogawas11234649@icloud.com>
2021-11-17 19:24:58 +0900

    デバイス関連の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,20 @@
 class User < ApplicationRecord
-  PASSWORD_REGEX = /\A[a-z0-9]+\z/.freeze
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, { uniqueness: true, presence: true, inclusion: { in: ['@'] } }
-  validates :nickname, presence: true
-  validates :encrypted_password, { presence: true, length: { minimum:6 }, format: { with: PASSWORD_REGEX } }
-  validates :password_confirmation, { presence: true, format: { with: PASSWORD_REGEX } }
+  validates :email,                 presence: true, uniqueness: true, inclusion: { in: ['@'] }
+  validates :nickname,              presence: true
+  validates :password,              presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :password_confirmation, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :last_name,             presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :name,                  presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :name_kana,             presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana,        presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday,              presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-    validates :name,      presence: true
-    validates :last_name, presence: true
-  end
-  
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
-    validates :name_kana,      presence: true
-    validates :last_name_kana, presence: true
-  end
-
-  validates :birthday, presence: true
+  has_many :items
+  has_many :buyers
 end
+
