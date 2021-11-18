5fa075a19da876cd184f148d156183c10b8f322c
ogawashintaro <ogawas11234649@icloud.com>
2021-11-18 18:37:24 +0900

    ユーザー管理機能の是正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   validates :nickname,              presence: true
   validates :password,              format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :password_confirmation, presence: true
   validates :last_name,             presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :name,                  presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :name_kana,             presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
