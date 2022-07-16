754612f1450d1fced1469c0ee0deac0856cf8ba5
ogawashintaro <ogawas11234649@icloud.com>
2022-07-16 21:48:45 +0900

    商品削除機能是正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item,           only: [:show, :edit, :update]
+  before_action :move_to_index,      only: :edit
 
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -23,9 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id != @item.user_id
-      redirect_to root_path
-    end
   end
 
   def update
@@ -47,13 +45,19 @@ class ItemsController < ApplicationController
     end
   end
 
-  private
 
+  private
   def item_params
     params.require(:item).permit(:item_name, :price, :information, :category_id, :delivery_fee_id, :area_id, :shipping_date_id, :product_condition_id, :image).merge(user_id: current_user.id)
   end
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 end
