206b8d59fc05ebf2ccb8a6cd06229a617971398d
ogawashintaro <ogawas11234649@icloud.com>
2022-08-03 15:26:14 +0900

    秘密鍵、公開鍵を環境変数へ変更

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,17 +6,23 @@ class PurchasesController < ApplicationController
 
   def create
     @purchase = Purchase.new(purchase_params)
+    @item = Item.find(params[:item_id])
     if @purchase.valid?
-      @purchase.save
-      redirect_to action: :index
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
+      redirect_to root_path
     else
       render :index
     end
   end
 
   private
-
+  
   def purchase_params
-    params.require(:purchase).permit(:postal_code, :area_id, :municipalities, :address, :building_name, :tel, :item_id).merge(user_id: current_user.id)
+    params.require(:purchase).permit(:postal_code, :area_id, :municipalities, :address, :building_name, :tel).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
   end
 end
